acabfbdde06385411821b4287c0a3cbcd561ea36
LiskIsBest <Darienmoore2002@gmail.com>
2022-11-23 08:23:27 -0600

    working on config setup

--- config.py
+++ config.py
@@ -2,8 +2,28 @@ import json
 
 config_file = open("config.json","w")
 
+alphabet = [chr(a) for a in range(97,121,1)]   
+
+def invalid_response(reason)->str:
+    print("Invalid response! - " + reason)
+
+def set_keys(key_count):
+    keys = dict()
+    for i in range(1,key_count+1):
+        new_key = input(f"Key {i} - Enter a single character (ex: a): ")
+        while(len(new_key)>1):
+            invalid_response("Only enter a single character (ex: a): ")
+            new_key = input(f"Key {i} - Enter a single character (ex: a): ")
+        keys[f"key{i}"] = new_key
+    json.dump(keys, config_file, indent=2)
+
+    
 key_count = int(input("How many keys? 4 or 8: "))
 while(key_count not in (4,8)):
-    print("Invalid response! ",end="")
+    invalid_response("Only valid options are 4 or 8")
     key_count = int(input("How many keys? 4 or 8: "))
+
+set_keys(key_count=key_count)
+
+print("keys set")
 
