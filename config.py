28a63a8d8dd1e69c6d53c54231cba97a6c8c9b22
LiskIsBest <Darienmoore2002@gmail.com>
2022-11-25 11:03:33 -0600

    poopy stinky

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import json
+import numpy as np
 
 config_file = open("config.json","w")
 
-alphabet = [chr(a) for a in range(97,121,1)]   
+alphabet = np.array([chr(a) for a in range(65,91,1)])
+
+#TODO change to pynput as to read special keys like control_R
 
 def invalid_response(reason)->str:
     print("Invalid response! - " + reason)
@@ -10,11 +13,11 @@ def invalid_response(reason)->str:
 def set_keys(key_count):
     keys = dict()
     for i in range(1,key_count+1):
-        new_key = input(f"Key {i} - Enter a single character (ex: a): ")
+        new_key = input(f"Key {i} - Enter a single character (ex: a): ") 
         while(len(new_key)>1):
-            invalid_response("Only enter a single character (ex: a): ")
+            invalid_response("Only enter a single/valid character (ex: a): ")
             new_key = input(f"Key {i} - Enter a single character (ex: a): ")
-        keys[f"key{i}"] = new_key
+        keys[f"key{i}"] = new_key.lower() if new_key in alphabet else new_key
     json.dump(keys, config_file, indent=2)
 
     
--- key_tester.py
+++ key_tester.py
@@ -1,12 +1,20 @@
 import tkinter as tk
+import json
+
+dump_json = open("dump.json", "w")
+
+to_dump = dict()
 
 def event_handle(event):
+    pressed_key = event.keysym
     pressed_check = "pressed" if event.type == "2" else "released"
-    print(new_text := f"Key: {event.keysym} was {pressed_check}")
+    print(new_text := f"Key: {pressed_key} was {pressed_check}")
+    to_dump[f"{pressed_key}"] = pressed_key
     label1["text"] = new_text + " - Press Esc to quit."
     return
 
 def exit_app(event):
+    json.dump(to_dump, dump_json, indent=2)
     root.destroy()
 
 root = tk.Tk()
